bc2adf595f09d4ee55a18a66cb1b9c657afc424b
Fernando Garcia <fgarcia1401@gmail.com>
2019-07-09 23:24:11 -0300

    TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Hello Flutter"),
+          title: Text("Hello Flutter",),
           centerTitle: true,
         ),
         body: Container(
           color: Colors.white,
+          child: Center(
+            child: Text("Hello World",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.red,
+              decorationStyle: TextDecorationStyle.dotted
+            ),) ,
+          )
         ));
   }
 }
